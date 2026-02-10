da17768db03f780208a05ecc029423a934589790
Abbas Gholami <abbas.gholami71@gmail.com>
2026-02-10 18:13:01 +0100

    fix tests

--- tests/schema_packages/test_schema_package.py
+++ tests/schema_packages/test_schema_package.py
@@ -1,6 +1,11 @@
 from nomad.client import normalize_all
 from nomad.datamodel import EntryArchive
-from pdi_nomad_plugin_rheed.schema_packages.schema_package import ELNRHEEDMeasurement, RHEEDPointScan
+
+from pdi_nomad_plugin_rheed.schema_packages.schema_package import (
+    ELNRHEEDMeasurement,
+    RHEEDPointScan,
+)
+
 
 def test_schema_instantiation():
     """
@@ -8,17 +13,17 @@ def test_schema_instantiation():
     """
     # 1. Create the Scan section
     scan = RHEEDPointScan()
-    scan.data_file = "dummy.csv"
-    
+    scan.data_file = 'dummy.csv'
+
     # 2. Wrap it in the main ELN Entry (since PointScan is now a subsection)
     entry = ELNRHEEDMeasurement()
     entry.point_scan = scan
-    
+
     # 3. Create Archive
     archive = EntryArchive(data=entry)
-    
+
     # 4. Normalize
     normalize_all(archive)
-    
+
     # 5. Verify
-    assert archive.data.point_scan.data_file == "dummy.csv"
+    assert archive.data.point_scan.data_file == 'dummy.csv'
